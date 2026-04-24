649732f3ea1049104004215cf134cedc0ea140ce
ChubbaTheHutt <124078662+ChubbaTheHutt@users.noreply.github.com>
2026-04-24 01:39:18 -0400

    Text Editor Memory
    Adding undo redo to text editor. Basic Idea.

---
+++ simpleTextEditor/src/main/ActionMemory.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayDeque;
+
+
+
+public class ActionMemory {
+
+    private ArrayDeque undoArrayDeque;
+    private ArrayDeque redoArrayDeque;
+    private int ACTION_MEMORY_SIZE = 3;
+
+    public ActionMemory() {
+        undoArrayDeque = new ArrayDeque(ACTION_MEMORY_SIZE);
+        redoArrayDeque = new ArrayDeque(ACTION_MEMORY_SIZE);       
+    }
+
+    public void redo(){
+        //remove from redo add to undo
+    }
+
+    public void undo(){
+        //remove from undo add to redo
+    }
+
+    public void pushAction(String s){
+        //add to undo, clear redo
+        if(!redoArrayDeque.isEmpty()){
+            redoArrayDeque = new ArrayDeque(ACTION_MEMORY_SIZE);
+        }
+    }
+
+    public boolean isEmpty(){
+        boolean isempty = undoArrayDeque.isEmpty() && redoArrayDeque.isEmpty();
+        return isempty;
+    }    
+
+}
